ca5b9237d2d2dab78895c6d09711e5fa49af3f1e
Christina <christinahkim1@gmail.com>
2021-03-25 23:45:41 +0000

    running for new mach

---
+++ kud.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [[ $1 == 'train' ]]; then
+    echo 'Run training...'
+    python train.py \
+        --cuda \
+        --data  /home/christina/$2 \
+        --checkpoints_dir /home/christina/models/,
+        --dataset $2 \
+        --model_size $3 \
+        --n_ctx 1024 \
+        --dropout 0.0 \
+        --dropatt 0.0 \
+        --optim adam \
+        --clip 1.0 \
+        --batch_size 512 \
+        --mini_batch_size 8 \
+        --warmup_step 500 \
+        --max_step 250000 \
+        --n_gpus $4  \
+        --eval_batch_size 2 \
+        --max_eval_steps 2 \
+        --max_epoch 10000 \
+        --local \
+        --note $5 \
+        --eval_interval $6 \
+        --limit_train_batches $7
+
+
+elif [[ $1 == 'eval' ]]; then
+    echo 'Run evaluation...'
+else
+    echo 'unknown argment 1'
+fi
