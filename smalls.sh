8b18c4fd7ed6104ee61e7f96e0a359dbbf07e812
Christina <christinahkim1@gmail.com>
2021-04-06 01:20:31 +0000

    smalls

---
+++ smalls.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+python train.py \
+    --cuda \
+    --data  /$1/$2 \
+    --checkpoints_dir /$1/models \
+    --dataset $2 \
+    --model_size $3 \
+    --n_ctx 1024 \
+    --dropout 0.0 \
+    --dropatt 0.0 \
+    --optim adam \
+    --clip 1.0 \
+    --batch_size 1 \
+    --mini_batch_size 8 \
+    --warmup_step 500 \
+    --max_step 250000 \
+    --n_gpus $4  \
+    --eval_batch_size 2 \
+    --max_eval_steps 2 \
+    --max_epoch 10000 \
+    --note $5 \
+    --local \
+    --save_dir /mnt/wandb \
+    --eval_interval $6 \
+    --limit_train_batches $6 \
+    --finetune -1
+
+python train.py \
+    --cuda \
+    --data  /$1/$2 \
+    --checkpoints_dir /$1/models \
+    --dataset $2 \
+    --model_size $3 \
+    --n_ctx 1024 \
+    --dropout 0.0 \
+    --dropatt 0.0 \
+    --optim adam \
+    --clip 1.0 \
+    --batch_size 1 \
+    --mini_batch_size 8 \
+    --warmup_step 500 \
+    --max_step 250000 \
+    --n_gpus $4  \
+    --eval_batch_size 2 \
+    --max_eval_steps 2 \
+    --max_epoch 10000 \
+    --note $5 \
+    --local \
+    --save_dir /mnt/wandb \
+    --eval_interval $6 \
+    --limit_train_batches $6 \
+    --finetune 1
